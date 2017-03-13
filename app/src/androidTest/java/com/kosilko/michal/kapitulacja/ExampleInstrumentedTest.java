a18be1c9be6708cfd788ee224a5c397768e2fa13
uczen <leszek466@gmail.com>
2017-03-13 17:23:34 +0100

    gITHUB

---
+++ app/src/androidTest/java/com/kosilko/michal/kapitulacja/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.kosilko.michal.kapitulacja;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.kosilko.michal.kapitulacja", appContext.getPackageName());
+    }
+}
